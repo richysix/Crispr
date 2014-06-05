b802f2fa4c5f64ed994571d26d6a3c0a57339207
Richard White <richard.white@sanger.ac.uk>
2014-06-05 09:22:10 +0100

    Minted new Dist::Zilla distribution
    lib/Crispr.pm
    Crispr - Main object for designing CRISPR/Cas9 guide RNAs.

---
+++ lib/Crispr.pm
@@ -0,0 +1,13 @@
+## no critic (RequireUseStrict, RequireUseWarnings, RequireTidyCode)
+package Crispr;
+## use critic
+
+# ABSTRACT: Abstract
+
+use warnings;
+use strict;
+use autodie;
+use Carp;
+
+
+1;
