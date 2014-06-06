afb8f05a38fad537f3b75ed8046cebfcf30ef5dc
Richard White <richard.white@sanger.ac.uk>
2014-06-06 11:12:17 +0100

    lib/Crispr/Target.pm Target object - representing a target piece of DNA
    t/target.t
    Test file for Target.pm

---
+++ lib/Crispr/Target.pm
@@ -0,0 +1,687 @@
+## no critic (RequireUseStrict, RequireUseWarnings, RequireTidyCode)
+package Crispr::Target;
+## use critic
+
+# ABSTRACT: Target object - representing a target piece of DNA
+
+use namespace::autoclean;
+use Moose;
+use Moose::Util::TypeConstraints;
+use Crispr::crRNA;
+use Carp;
+use Scalar::Util qw( weaken );
+use DateTime;
+
+my $debug = 0;
+
+enum 'Crispr::Target::Strand', [qw( 1 -1 )];
+
+subtype 'Crispr::Target::NOT_EMPTY',
+    as 'Str',
+    where { $_ ne '' },
+    message { "Attribute is empty!\n" };
+
+=method new
+
+  Usage       : my $target = Crispr::target->new(
+					target_id => undef,
+                    name => 'hspa5_exon1',
+                    assembly => 'Zv9',
+					chr => '5',
+					start => 20103030,
+					end => 20103930,
+					strand => '1',
+					species => 'zebrafish',
+					requires_enzyme => y,
+					gene_id => 'ENSDARG00000004665',
+					gene_name => 'hspa5,
+					requestor => 'richard.white',
+					ensembl_version => 75,
+					designed => '2014-03-02',
+                    crRNAs => \@crRNAs,
+					target_adaptor => $target_adaptor,
+                );
+  Purpose     : Constructor for creating target objects
+  Returns     : Crispr::target object
+  Parameters  : target_id => Int
+                name => String
+                assembly => String
+                chr => String
+                start => Int
+                end => Int
+                strand => '1' or '-1'
+                species => String
+                requires_enzyme => 1, 0, 'y' or 'n'
+                gene_id => String
+                gene_name => String
+                requestor => String
+                ensembl_version => Int
+                designed => DateTime or String (yyyy-mm-dd)
+                crRNAs => ArrayRef of Crispr::crRNA objects
+				target_adaptor => Crispr::Adaptors::targetAdaptor,
+  Throws      : If parameters are not the correct type
+  Comments    : None
+
+=cut
+
+=method target_id
+
+  Usage       : $target->target_id;
+  Purpose     : Getter for target_id (database id) attribute
+  Returns     : Int (can be undef)
+  Parameters  : Int
+  Throws      : 
+  Comments    : 
+
+=cut
+
+has 'target_id' => (
+    is => 'rw',
+    isa => 'Maybe[Int]',
+);
+
+=method name
+
+  Usage       : $target->name;
+  Purpose     : Getter for name attribute
+  Returns     : String
+  Parameters  : 
+  Throws      : If input is given
+  Comments    : 
+
+=cut
+
+has 'name' => (
+    is => 'ro',
+    isa => 'Crispr::Target::NOT_EMPTY',
+);
+
+=method assembly
+
+  Usage       : $target->assembly;
+  Purpose     : Getter for assembly attribute
+  Returns     : String
+  Parameters  : 
+  Throws      : If input is given
+  Comments    : 
+
+=cut
+
+has 'assembly' => (
+    is => 'ro',
+    isa => 'Maybe[Str]',
+);
+
+=method chr
+
+  Usage       : $target->chr;
+  Purpose     : Getter for chr attribute
+  Returns     : String
+  Parameters  : 
+  Throws      : If input is given
+  Comments    : 
+
+=cut
+
+has 'chr' => (
+    is => 'ro',
+    isa => 'Maybe[Str]',
+);
+
+=method start
+
+  Usage       : $target->start;
+  Purpose     : Getter for start attribute
+  Returns     : Int
+  Parameters  : 
+  Throws      : If input is given
+  Comments    : 
+
+=cut
+
+=method end
+
+  Usage       : $target->end;
+  Purpose     : Getter for end attribute
+  Returns     : Int
+  Parameters  : 
+  Throws      : If input is given
+  Comments    : 
+
+=cut
+
+# enforce start < end - NEED TO IMPLEMENT
+has [ 'start', 'end' ] => (
+    is => 'ro',
+    isa => 'Int',
+);
+
+=method strand
+
+  Usage       : $target->strand;
+  Purpose     : Getter for strand attribute
+  Returns     : String
+  Parameters  : 
+  Throws      : If input is given
+  Comments    : 
+
+=cut
+
+has 'strand' => (
+    is => 'ro',
+    isa => 'Crispr::Target::Strand',
+    default => '1',
+);
+
+=method species
+
+  Usage       : $target->species;
+  Purpose     : Getter for species attribute
+  Returns     : String
+  Parameters  : 
+  Throws      : If input is given
+  Comments    : 
+
+=cut
+
+has 'species' => (
+    is => 'rw',
+    isa => 'Maybe[Str]',
+);
+
+=method requires_enzyme
+
+  Usage       : $target->requires_enzyme;
+  Purpose     : Getter for requires_enzyme attribute
+  Returns     : Bool
+  Parameters  : Bool    1 for y, 0 for n
+  Throws      : 
+  Comments    : 
+
+=cut
+
+has 'requires_enzyme' => (
+    is => 'rw',
+    isa => 'Bool',
+    default => 0,
+);
+
+=method gene_id
+
+  Usage       : $target->gene_id;
+  Purpose     : Getter for gene_id attribute
+  Returns     : String
+  Parameters  : 
+  Throws      : If input is given
+  Comments    : 
+
+=cut
+
+has 'gene_id' => (
+    is => 'ro',
+    isa => 'Maybe[Str]',
+);
+
+=method gene_name
+
+  Usage       : $target->gene_name;
+  Purpose     : Getter for gene_name attribute
+  Returns     : String
+  Parameters  : 
+  Throws      : If input is given
+  Comments    : 
+
+=cut
+
+has 'gene_name' => (
+    is => 'ro',
+    isa => 'Maybe[Str]',
+);
+
+=method requestor
+
+  Usage       : $target->requestor;
+  Purpose     : Getter for requestor attribute
+  Returns     : String
+  Parameters  : 
+  Throws      : If input is given
+  Comments    : 
+
+=cut
+
+has 'requestor' => (
+    is => 'ro',
+    isa => 'Maybe[Str]',
+);
+
+=method ensembl_version
+
+  Usage       : $target->ensembl_version;
+  Purpose     : Getter for ensembl_version attribute
+  Returns     : String
+  Parameters  : 
+  Throws      : If input is given
+  Comments    : 
+
+=cut
+
+has 'ensembl_version' => (
+    is => 'ro',
+    isa => 'Maybe[Int]',
+);
+
+=method designed
+
+  Usage       : $target->designed;
+  Purpose     : Getter/Setter for designed attribute
+  Returns     : DateTime
+  Parameters  : Either DateTime object or Str of form yyyy-mm-dd
+  Throws      : 
+  Comments    : 
+
+=cut
+
+has 'designed' => (
+    is => 'rw',
+    isa => 'Maybe[DateTime]',
+    default => undef,
+);
+
+=method crRNAs
+
+  Usage       : $target->crRNAs;
+  Purpose     : Getter/Setter for crRNAs attribute
+  Returns     : ArrayRef of Crispr::crRNA objects
+  Parameters  : ArrayRef of Crispr::crRNA objects
+  Throws      : 
+  Comments    : 
+
+=cut
+
+has 'crRNAs' => (
+    is => 'rw',
+    isa => 'ArrayRef[Crispr::crRNA]',
+);
+
+=method target_adaptor
+
+  Usage       : $target->target_adaptor;
+  Purpose     : Getter/Setter for target_adaptor attribute
+  Returns     : Crispr::Adaptors::TargetAdaptor
+  Parameters  : Crispr::Adaptors::TargetAdaptor
+  Throws      : 
+  Comments    : 
+
+=cut
+
+has 'target_adaptor' => (
+    is => 'rw',
+    isa => 'Crispr::Adaptors::TargetAdaptor',
+);
+
+around BUILDARGS => sub{
+    my $method = shift;
+    my $self = shift;
+    my %args;
+    
+    if( !ref $_[0] ){
+        for( my $i = 0; $i < scalar @_; $i += 2){
+            my $k = $_[$i];
+            my $v = $_[$i+1];
+            if( $k eq 'designed' ){
+                if( defined $v && ( !ref $v || ref $v ne 'DateTime' ) ){
+                    my $date_obj = $self->_parse_date( $v );
+                    $v = $date_obj;
+                }
+            }
+            elsif( $k eq 'chr' && ( defined $v && $v eq '' ) ){
+                $v = undef;
+            }
+			elsif( $k eq 'strand' && ( $v ne '1' && $v ne '-1' ) ){
+				my $strand = $self->_parse_strand_input( $v );
+				$v = $strand;
+			}
+			elsif( $k eq 'requires_enzyme' && ( !defined $v || ( $v ne '1' && $v ne '0' ) ) ){
+				my $requires_enzyme = $self->_parse_requires_enzyme_input( $v );
+				$v = $requires_enzyme;
+			}
+			elsif( $k eq 'crRNAs' ){
+				foreach my $crRNA_ref ( @{$v} ){
+					weaken( $crRNA_ref );
+				}
+			}
+            $args{ $k } = $v;
+        }
+        return $self->$method( \%args );
+    }
+    elsif( ref $_[0] eq 'HASH' ){
+        if( exists $_[0]->{'designed'} ){
+            if( defined $_[0]->{'designed'} &&
+                ( !ref $_[0]->{'designed'} || ref $_[0]->{'designed'} ne 'DateTime' ) ){
+                my $date_obj = $self->_parse_date( $_[0]->{'designed'} );
+                $_[0]->{'designed'} = $date_obj;
+            }
+        }
+        if( exists $_[0]->{'chr'} && ( defined $_[0]->{'chr'} && $_[0]->{'chr'} eq '' ) ){
+            $_[0]->{'chr'} = undef;
+        }
+        if( exists $_[0]->{'strand'} && ( $_[0]->{'strand'} ne '1' && $_[0]->{'strand'} ne '-1' ) ){
+			my $strand = $self->_parse_strand_input( $_[0]->{'strand'} );
+			$_[0]->{'strand'} = $strand;
+        }
+        if( exists $_[0]->{'requires_enzyme'} &&
+            ( !defined $_[0]->{'requires_enzyme'} ||
+            ( $_[0]->{'requires_enzyme'} ne '1' &&
+                $_[0]->{'requires_enzyme'} ne '0' ) ) ){
+			my $requires_enzyme = $self->_parse_requires_enzyme_input( $_[0]->{'requires_enzyme'} );
+			$_[0]->{'requires_enzyme'} = $requires_enzyme;
+        }
+		if( exists $_[0]->{'crRNAs'} ){
+			foreach my $crRNA_ref ( @{$_[0]->{'crRNAs'}} ){
+				weaken( $crRNA_ref );
+			}
+		}
+        return $self->$method( $_[0] );
+    }
+    else{
+        confess "method new called without Hash or Hashref.\n";
+    }
+};
+
+#_parse_strand_input
+#
+#Usage       : $crRNA->_parse_strand_input( '+' );
+#Purpose     : accepts '+' or '-' and converts to '1' or '-1'
+#				retruns undef for anything else
+#Returns     : '1' OR '-1'
+#Parameters  : String
+#Throws      : 
+#Comments    :
+
+sub _parse_strand_input {
+	my ( $self, $strand ) = @_;
+	if( $strand eq '+' ){
+		return '1';
+	}
+	elsif( $strand eq '-' ){
+		return '-1';
+	}
+	else{
+		return undef;
+	}
+}
+
+# around requires_enzyme
+# This is to convert the underlying Boolean value into either y or n
+#
+around 'requires_enzyme' => sub {
+    my ( $method, $self, $input ) = @_;
+    if( defined $input ){
+        my $req_enzyme = $self->_parse_requires_enzyme_input($input);
+        if( !defined $req_enzyme ){
+            warn "The value for $input is not a recognised value. Should be one of 1, 0, y or n.\n";
+            return $self->$method();
+        }
+        return $self->$method( $req_enzyme );
+    }
+    else{
+        my $output = $self->$method();
+        if( $output ){
+            return 'y';
+        }
+        else {
+            return 'n';
+        }
+    }
+};
+
+## around crRNAs
+## This is to weaken references to crRNAs to prevent circular references
+##
+#around 'crRNAs' => sub {
+#    my ( $method, $self, $input ) = @_;
+#    if( $input ){
+#		foreach my $crRNA ( @{$input} ){
+#            if( defined $crRNA->target ){
+#                weaken( $crRNA );
+#            }
+#		}
+#        return $self->$method( $input );
+#    }
+#    else{
+#		return $self->$method();
+#    }
+#};
+#
+#_parse_requires_enzyme_input
+#
+#Usage       : $crRNA->_parse_requires_enzyme_input( '+' );
+#Purpose     : Accepts either 0, 1, n, and y
+#               Converts y to 1 and n to 0.
+#               1 and 0 remain unchanged.
+#               Anything else is converted to 0.
+#               Default value is 1
+#Returns     : 1 OR 0
+#Parameters  : String
+#Throws      : 
+#Comments    :
+
+sub _parse_requires_enzyme_input {
+	my ( $self, $requires_enzyme ) = @_;
+	my $enzyme = $requires_enzyme eq '1'	    ?	1
+		:		$requires_enzyme eq '0'		?	0
+		:		$requires_enzyme eq 'y'		?	1
+		:		$requires_enzyme eq 'n'		?	0
+		:										undef
+		;
+	return $enzyme;
+}
+
+=method region
+
+  Usage       : $target->region;
+  Purpose     : Getter for region attribute
+  Returns     : String ( [CHR:]START-END:STRAND )
+  Parameters  : None
+  Throws      : 
+  Comments    : 
+
+=cut
+
+sub region {
+    my ( $self, ) = @_;
+    
+    my $region;
+    if( !defined $self->chr ){
+        $region = join('-', $self->start, $self->end, ) . ":" . $self->strand;
+    }
+    else{
+        $region = join(':',
+            $self->chr,
+            join('-',
+                $self->start,
+                $self->end,
+            ),
+            $self->strand,
+        );
+    }
+    return $region;
+}
+
+=method length
+
+  Usage       : $target->length;
+  Purpose     : Returns the length of the target sequence
+  Returns     : Int
+  Parameters  : None
+  Throws      : 
+  Comments    : 
+
+=cut
+
+sub length {
+	my ( $self, ) = @_;
+	return $self->end - ($self->start - 1);
+}
+
+=method summary
+
+  Usage       : $target->summary;
+  Purpose     : Returns a short summary of the target info
+  Returns     : Array
+  Parameters  : None
+  Throws      : 
+  Comments    : undef attributes are replaced with NULL
+
+=cut
+
+sub summary {
+    my ( $self, ) = @_;
+    my @info = ( $self->name, );
+    push @info, ( $self->gene_id || 'NULL' );
+    push @info, ( $self->gene_name || 'NULL' );
+    push @info, ( $self->requestor || 'NULL' );
+    
+    return @info;
+}
+
+=method info
+
+  Usage       : $target->info;
+  Purpose     : Returns target info
+  Returns     : Array
+  Parameters  : None
+  Throws      : 
+  Comments    : undef attributes are replaced with NULL
+
+=cut
+
+sub info {
+    my ( $self, ) = @_;
+    my @info;
+    
+    push @info, ( $self->target_id || 'NULL' );
+    push @info,   $self->name;
+    push @info, ( $self->assembly || 'NULL' );
+    push @info, ( $self->chr || 'NULL' );
+    push @info,   $self->start;
+    push @info,   $self->end;
+    push @info,   $self->strand;
+    push @info, ( $self->species || 'NULL');
+    push @info,   $self->requires_enzyme;
+    push @info, ( $self->gene_id || 'NULL');
+    push @info, ( $self->gene_name || 'NULL');
+    push @info, ( $self->requestor || 'NULL' );
+    push @info, ( $self->ensembl_version || 'NULL' );
+    push @info, ( $self->designed || 'NULL' );
+    
+    return @info;
+}
+
+# around designed
+# This is to accept either a DateTime object or a string in form yyyy-mm-dd
+#
+around 'designed' => sub {
+    my ( $method, $self, $input ) = @_;
+    my $date_obj;
+    
+    if( $input ){
+        #is the input already a DateTime object
+        if( ref $input eq 'DateTime' ){
+            $date_obj = $input;
+        }
+        else{
+            # parse date info
+            $date_obj = $self->_parse_date( $input );
+        }
+        return $self->$method( $date_obj );
+    }
+    else{
+        if( defined $self->$method ){
+            return $self->$method->ymd;
+        }
+        else{
+            return $self->$method;
+        }
+    }
+};
+
+#_parse_date
+#
+#Usage       : $crRNA->_parse_date( '2014-03-21' );
+#Purpose     : Converts dates in form yyyy-mm-dd into DateTime object
+#Returns     : DateTime object
+#Parameters  : String
+#Throws      : If date is not in correct format
+#Comments    :
+
+sub _parse_date {
+    my ( $self, $input ) = @_;
+    my $date_obj;
+    
+    if( $input =~ m/\A([0-9]{4})-([0-9]{2})-([0-9]{2})\z/xms ){
+        $date_obj = DateTime->new(
+            year       => $1,
+            month      => $2,
+            day        => $3,
+        );
+    }
+    else{
+        confess "The date supplied is not a valid format\n";
+    }
+    return $date_obj;
+}
+
+
+__PACKAGE__->meta->make_immutable;
+1;
+
+##########################
+# DOCUMENTATION
+##########################
+
+
+=head1 NAME
+ 
+<Crispr::Target> - <Module for Crispr::Target objects.>
+
+ 
+=head1 SYNOPSIS
+ 
+    use Crispr::Target;
+    my $target = Crispr::Target->new(
+        name => 'SLC39A14',
+        assembly => 'Zv9',
+        chr => '5',
+        start => 18067321,
+        end => 18083466,
+        strand => '-1',
+        species => 'danio_rerio',
+        requires_enzyme => y,
+        gene_id => 'ENSDARG00000090174',
+        gene_name => 'SLC39A14',
+        requestor => 'richard.white',
+        ensembl_version => 75,
+        designed => '2014-03-02',
+    );
+    
+    # print out target summary or info
+    print join("\t", $target->summary ), "\n";
+    print join("\t", $target->info ), "\n";
+    
+
+=head1 DESCRIPTION
+ 
+Object of this class represent a targets strecth of DNA to search for potential crispr target sites. 
+ 
+=head1 DIAGNOSTICS
+ 
+ 
+=head1 CONFIGURATION AND ENVIRONMENT
+ 
+ 
+=head1 DEPENDENCIES
+ 
+ 
+=head1 INCOMPATIBILITIES
+ 
---
+++ t/target.t
@@ -0,0 +1,241 @@
+#!/usr/bin/env perl
+# target.t
+use Test::More;
+use Test::Exception;
+use Test::Warn;
+use Test::MockObject;
+use English qw( -no_match_vars );
+use Data::Dumper;
+
+use DateTime;
+#get current date
+my $date_obj = DateTime->now();
+my $todays_date = $date_obj->ymd;
+
+plan tests => 4 + 2 + 1 + 17 + 4 + 4 + 2 + 5 + 6 + 6 + 2 + 8 + 2 + 2 + 2 + 2 + 6 + 8 + 4;
+
+my $species = 'zebrafish';
+
+use Crispr::Target;
+
+# make a new target - designed should be undef
+# have not defined crRNAs
+my $target = Crispr::Target->new(
+    name => 'SLC39A14',
+    assembly => 'Zv9',
+    chr => '5',
+    start => 18067321,
+    end => 18083466,
+    strand => '-1',
+    species => 'danio_rerio',
+    requires_enzyme => 1,
+    gene_id => 'ENSDARG00000090174',
+    gene_name => 'SLC39A14',
+    requestor => 'crispr_test',
+    ensembl_version => 71,
+); 
+
+## make a new Mock cRNAs object
+use Crispr::crRNA;
+my @crRNAs;
+for ( 1..3 ){
+    my $crRNA = Test::MockObject->new();
+    $crRNA->set_isa('Crispr::crRNA');
+    $crRNA->mock( 'target', sub { return $target } );
+    #my $crRNA = Crispr::crRNA->new(
+    #    target => $target,
+    #);
+    push @crRNAs, $crRNA;
+}
+$target->crRNAs( \@crRNAs );
+
+# check crRNAs - 4 tests
+isa_ok( $target->crRNAs, 'ARRAY', 'crRNAs' );
+#print Dumper( $target->crRNAs );
+#print Dumper( $target );
+foreach( @{ $target->crRNAs } ){
+    isa_ok( $_, 'Crispr::crRNA', 'crRNA' );
+}
+
+# make a new Mock target adaptor object
+my $ta = Test::MockObject->new();
+$ta->set_isa('Crispr::Adaptors::TargetAdaptor');
+$target->target_adaptor( $ta );
+
+# new target without a chr, assembly, strand, gene_id, gene_name, ensembl_version
+my $target_2 = Crispr::Target->new(
+    name => 'gfp_50_100',
+    start => 50,
+    end => 60,
+    species => 'Aequorea_victoria',
+    requestor => 'crispr_test',
+    designed => '2012-07-18',
+);
+
+# check target_adaptor - 2 tests
+isa_ok( $target->target_adaptor, 'Crispr::Adaptors::TargetAdaptor', 'target_adaptor');
+is( $target_2->target_adaptor, undef, 'target_adaptor2');
+
+# check Target and attributes
+# 1 test
+isa_ok( $target, 'Crispr::Target' );
+
+# check method calls 17 tests
+my @methods = qw( target_id name assembly chr start
+    end strand species requires_enzyme gene_id
+    gene_name requestor ensembl_version designed target_adaptor
+    region info );
+
+foreach my $method ( @methods ) {
+    can_ok( $target, $method );
+}
+
+# 4 tests - check type constraints for target_id
+is( $target->target_id, undef, 'Get id' );
+
+$tmp_target = Crispr::Target->new( target_id => '1' );
+is( $tmp_target->target_id, '1', 'Set id' );
+throws_ok { Crispr::Target->new( target_id => 'string') } qr/Validation failed/ms, 'string id';
+throws_ok { Crispr::Target->new( target_id => '' ) } qr/Validation failed/ms, 'Empty string id';
+
+# 4 tests - check type constraints for name
+is( $target->name, 'SLC39A14', 'Get name' );
+throws_ok { Crispr::Target->new( name => '' ) } qr/Attribute is empty/, 'empty name';
+$tmp_target = Crispr::Target->new( name => 1 ); #coerced to string '1'
+is( $tmp_target->name, '1', 'Number as string');
+is( $target_2->name, 'gfp_50_100', 'Name 2');
+
+# assembly - 2 tests
+is( $target->assembly, 'Zv9', 'Assembly1');
+is( $target_2->assembly, undef, 'Assembly2');
+
+# chr - 5 tests
+is( $target->chr, '5', 'Get chr' );
+$tmp_target = Crispr::Target->new( chr => '' );
+is( $tmp_target->chr, undef, 'Empty string chr' );
+$tmp_target = Crispr::Target->new( chr => 'Zv9_NA' );
+is( $tmp_target->chr, 'Zv9_NA', 'Set chr Zv9_NA');
+$tmp_target = Crispr::Target->new( chr => '5' );
+is( $tmp_target->chr, '5', 'Set chr 5');
+
+is( $target_2->chr, undef, 'Get undef chr' );
+
+# start and end - 6 tests
+is( $target->start, 18067321, 'Get start' );
+is( $target->end, 18083466, 'Get end' );
+throws_ok { Crispr::Target->new( start => '' ) } qr/Validation failed/, 'Empty start';
+throws_ok { Crispr::Target->new( end => '' ) } qr/Validation failed/, 'Empty end';
+is( $target_2->start, 50, 'Start2');
+is( $target_2->end, 60, 'Start2');
+
+# check strand - 6 tests
+is( $target->strand, '-1', 'Get strand');
+# check default value
+is( $tmp_target->strand, '1', 'Check default strand' );
+throws_ok { Crispr::Target->new( strand => '2' ) } qr/Validation failed/, 'Not a Strand';
+is( $target_2->strand, '1', 'Strand2');
+# check parse_strand
+$tmp_target = Crispr::Target->new( strand => '+' );
+is( $tmp_target->strand, '1', 'Check parse + strand' );
+$tmp_target = Crispr::Target->new( strand => '-' );
+is( $tmp_target->strand, '-1', 'Check parse - strand' );
+
+# species - 2 tests
+is( $target->species, 'danio_rerio', 'species1');
+is( $target_2->species, 'Aequorea_victoria', 'species2');
+
+# requires_enzyme - 8 tests
+is( $target->requires_enzyme, 'y', 'Enzyme1');
+is( $target_2->requires_enzyme, 'n', 'check default Enzyme');
+$target->requires_enzyme('n');
+is( $target->requires_enzyme, 'n', 'check setting enzyme with n');
+$target->requires_enzyme('y');
+is( $target->requires_enzyme, 'y', 'check setting enzyme with y');
+$target->requires_enzyme(0);
+is( $target->requires_enzyme, 'n', 'check setting enzyme with 0');
+$target->requires_enzyme(1);
+is( $target->requires_enzyme, 'y', 'check setting enzyme with 1');
+
+warning_like { $target->requires_enzyme('cheese') }
+    qr/The\svalue\sfor\s\S+\sis\snot\sa\srecognised\svalue.\sShould\sbe\sone\sof\s1,\s0,\sy\sor\sn./,
+    'check setting enzyme with something else warns';
+is( $target->requires_enzyme(undef), 'y', 'check setting enzyme to undef changes nothing');
+
+# gene_id - 2 tests
+is( $target->gene_id, 'ENSDARG00000090174', 'Gene id 1');
+is( $target_2->gene_id, undef, 'Gene id 2');
+
+# gene_name - 2 tests
+is( $target->gene_name, 'SLC39A14', 'Gene name 1');
+is( $target_2->gene_name, undef, 'Gene name 2');
+
+# requestor - 2 tests
+is( $target->requestor, 'crispr_test', 'requestor1');
+is( $target_2->requestor, 'crispr_test', 'requestor2');
+
+# ensembl_version - 2 tests
+is( $target->ensembl_version, 71, 'ensembl_version1');
+is( $target_2->ensembl_version, undef, 'ensembl_version2');
+
+# designed - 6 tests
+is( $target->designed, undef, 'check default date' );
+$target->designed( '2012-05-23' );
+is( $target->designed, '2012-05-23', 'check date set' );
+# set date with DateTime object
+$target->designed( $date_obj );
+is( $target->designed, $todays_date, 'check date set using DateTime object' );
+
+throws_ok { $target->designed( '20120228' ) } qr/valid format/, 'Invalid date format';
+throws_ok { $target->designed( '2012-02-30' ) } qr/Invalid/, 'Impossible date';
+
+is( $target_2->designed, '2012-07-18', 'designed2');
+
+# 8 tests - check output of non attribute methods
+is( $target->region, '5:18067321-18083466:-1', 'check region');
+is( $target_2->region, '50-60:1', 'Get region without chr');
+is( $target->length, 16146, 'check length' );
+
+like( join("\t", $target->summary ),
+    qr/SLC39A14\tENSDARG00000090174\tSLC39A14\tcrispr_test/, 'check summary 1' );
+like( join("\t", $target_2->summary ),
+    qr/gfp_50_100\tNULL\tNULL\tcrispr_test/, 'check summary 2' );
+like( join("\t", $target->info ),
+    qr/\A NULL\tSLC39A14\tZv9\t5\t18067321\t18083466\t-1\tdanio_rerio\ty\tENSDARG00000090174\tSLC39A14\tcrispr_test\t71\t$todays_date \z/xms, 'check info 1' );
+like( join("\t", $target_2->info ),
+    qr/\A NULL\tgfp_50_100\tNULL\tNULL\t50\t60\t1\tAequorea_victoria\tn\tNULL\tNULL\tcrispr_test\tNULL\t2012-07-18 \z/xms, 'check info 2' );
+
+$tmp_target = Crispr::Target->new(
+    target_id => 1,
+    name => 'SLC39A14',
+    assembly => 'Zv9',
+    chr => '5',
+    start => 18067321,
+    end => 18083466,
+    strand => '-1',
+    requires_enzyme => 1,
+    gene_id => 'ENSDARG00000090174',
+    gene_name => 'SLC39A14',
+    ensembl_version => 71,
+);
+
+like( join("\t", $tmp_target->info ),
+    qr/\A 1\tSLC39A14\tZv9\t5\t18067321\t18083466\t-1\tNULL\ty\tENSDARG00000090174\tSLC39A14\tNULL\t71\tNULL \z/xms, 'check info 3' );
+
+# create object with hash and hash_ref
+my %args = (
+    name => 'gfp_50_100',
+    start => 50,
+    end => 60,
+    species => 'Aequorea_victoria',
+    requestor => 'crispr_test',
+);
+
+my $target_3 = Crispr::Target->new( %args );
+my $target_4 = Crispr::Target->new( \%args );
+
+# check name and chr set properly - 4 tests
+is( $target_3->name, 'gfp_50_100', 'check name is set with hash calling style' );
+is( $target_4->name, 'gfp_50_100', 'check name is set with hashref calling style' );
+is( $target_3->chr, undef, 'check chr is set with hash calling style' );
+is( $target_4->chr, undef, 'check chr is set with hashref calling style' );
+
