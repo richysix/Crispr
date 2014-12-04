e2e49e5a24c38dd5924b993a02f8fdeab3e38fb0
Richard White <richard.white@sanger.ac.uk>
2014-12-04 16:53:25 +0000

    New Object PrimerAdaptor Adaptor object for storing Primers to an SQL database and retrieving them.
    New File: lib/Crispr/DB/PrimerAdaptor.pm - Primer Adaptor object
    New File: t/primer_adaptor.t - test file

---
+++ lib/Crispr/DB/PrimerAdaptor.pm
@@ -0,0 +1,452 @@
+## no critic (RequireUseStrict, RequireUseWarnings, RequireTidyCode)
+package Crispr::DB::PrimerAdaptor;
+## use critic
+
+# ABSTRACT: PrimerAdaptor object - object for storing Primer objects in and retrieving them from a SQL database
+
+use namespace::autoclean;
+use Moose;
+use Crispr::Primer;
+use Crispr::Plate;
+use English qw( -no_match_vars );
+use DateTime;
+use Readonly;
+
+extends 'Crispr::DB::BaseAdaptor';
+
+=method new
+
+  Usage       : my $primer_adaptor = Crispr::DB::PrimerAdaptor->new(
+					db_connection => $db_connection,
+                );
+  Purpose     : Constructor for creating primer adaptor objects
+  Returns     : Crispr::DB::PrimerAdaptor object
+  Parameters  :     db_connection => Crispr::DB::DBConnection object,
+  Throws      : If parameters are not the correct type
+  Comments    : The preferred method for constructing a PrimerAdaptor is to use the
+                get_adaptor method with a previously constructed DBConnection object
+
+=cut
+
+=method plate_adaptor
+
+  Usage       : $primer_adaptor->plate_adaptor;
+  Purpose     : Getter for a plate_adaptor
+  Returns     : Crispr::DB::PlateAdaptor
+  Parameters  : None
+  Throws      : If input is given
+  Comments    : 
+
+=cut
+
+has 'plate_adaptor' => (
+    is => 'ro',
+    isa => 'Crispr::DB::PlateAdaptor',
+    lazy => 1,
+    builder => '_build_plate_adaptor',
+);
+
+=method store
+
+  Usage       : $primer_adaptor->store;
+  Purpose     : method to store a primer in the database.
+  Returns     : 1 on Success.
+  Parameters  : Either Crispr::Primer
+                Or Labware::Well containing Crispr::Primer object
+  Throws      : If input is not correct type
+  Comments    : 
+
+=cut
+
+sub store {
+    my ( $self, $object ) = @_;
+    my $dbh = $self->connection->dbh();
+    
+    # check object
+    if( !$object ){
+        confess "Argument to store is empty. An object must be supplied in order to add oligos to the database!\n";
+    }
+    else{
+        # check if $object is either a Labware::Well object or a Crispr::Primer one
+        if( !ref $object ||
+           !($object->isa('Labware::Well') || $object->isa('Crispr::Primer') ) ){
+            confess join(q{ },
+                'The supplied object must be either a Labware::Well object',
+                'or a Crispr::Primer object, not', ref $object, ), "!\n";
+        }
+    }
+    
+    my ( $primer, $plate_id, $well_id, );
+    if( $object->isa('Labware::Well') ){
+        $primer = $object->contents; 
+        if( !$primer ){
+            confess join(q{ },
+                'The well is empty!',
+                'A Crispr::Primer object must be supplied to add to the database',
+            ), "!\n";
+        }
+        else{
+            # check $primer is a Crispr::Primer object
+            if( !ref $primer || !$primer->isa('Crispr::Primer') ){
+                confess join(q{ },
+                    'The supplied object must be a Crispr::Primer object, not',
+                    ref $primer,
+                ), "!\n";
+            }
+        }
+        # check plate exists - check_entry_exists_in_db inherited from DBAttributes.
+        my $check_plate_st = 'select count(*) from plate where plate_name = ?';
+        if( !$self->check_entry_exists_in_db( $check_plate_st, [ $object->plate->plate_name ] ) ){
+            # add plate to database
+            $self->plate_adaptor->store( $object->plate );
+        }
+        if( !$object->plate->plate_id ){
+            # fetch plate id from db
+            $plate_id = $self->plate_adaptor->get_plate_id_from_name( $object->plate->plate_name );
+        }
+        else{
+            $plate_id = $object->plate->plate_id;
+        }
+        $well_id = $object->position
+    }
+    if( $object->isa('Crispr::Primer') ){
+        $primer = $object;
+    }
+    
+    # check primer for tail
+    my ( $primer_seq, $primer_tail ) = $self->_split_primer_into_seq_and_tail( $primer->sequence );
+    
+    # insert primer statement
+    my $primer_statement = 'insert into primer values( ?, ?, ?, ?, ?, ?, ?, ?, ? );';
+    
+    $self->connection->txn(  fixup => sub {
+        my $sth = $dbh->prepare($primer_statement); 
+        $sth->execute(
+            undef, $primer_seq,
+            $primer->seq_region,
+            $primer->seq_region_start, $primer->seq_region_end,
+            $primer->seq_region_strand,
+            $primer_tail,
+            $plate_id, $well_id,
+        );
+        $sth->finish();
+        my $last_id = $dbh->last_insert_id( 'information_schema', $self->dbname(), 'primer', 'primer_id' );
+        $primer->primer_id( $last_id );
+    } );
+    return $primer;
+}
+
+sub _split_primer_into_seq_and_tail {
+    my ( $self, $primer_sequence, ) = @_;
+    my @tail_sequences = ( qw{ ACACTCTTTCCCTACACGACGCTCTTCCGATCT TCGGCATTCCTGCTGAACCGCTCTTCCGATCT } );
+    
+    my $primer_tail;
+    my $matches;
+    foreach( @tail_sequences ){
+        if( $primer_sequence =~ m/\A $_/xms ){
+            $matches++;
+            $primer_tail = $_;
+            $primer_sequence =~ s/\A $_//xms
+        }
+    }
+    if( $matches && $matches > 1 ){
+        die "Primer sequence matches more than one tail: $primer_sequence\n";
+    }
+    return ( $primer_sequence, $primer_tail, );
+}
+
+sub fetch_by_id {
+    my ( $self, $primer_id ) = @_;
+    my $dbh = $self->connection->dbh();
+    # select statement
+    my $statement = 'select * from primer where primer_id = ?';
+    my ( $primers, $num_rows ) = $self->_fetch_primers_by_attributes( $statement, [ $primer_id ] );
+    if( $num_rows == 0 ){
+        "Couldn't find primer:$primer_id in database.\n";
+    }
+    elsif( $num_rows > 1 ){
+        "Primer id:$primer_id should be unique, but got more than one row returned!\n";
+    }
+    else{
+        return $primers->[0];
+    }
+}
+
+sub fetch_by_name {
+    my ( $self, $primer_name ) = @_;
+    my $dbh = $self->connection->dbh();
+    # parse primer name
+    my ( $chr, $region, $strand ) = split /:/, $primer_name;
+    my ( $start, $end ) = split /-/, $region;
+    if( !$chr || !$region || !$strand || !$start || !$end ){
+        confess "Cannot retrieve primer from database using primer name, $primer_name!\n";
+    }
+    
+    # select statement
+    my $statement = 'select * from primer where chr = ? and $start = ? and end = ? and strand = ?;';
+    my ( $primers, $num_rows ) = $self->_fetch_primers_by_attributes( $statement, [ $chr, $start, $end, $strand ] );
+    if( $num_rows == 0 ){
+        "Couldn't find primer:$primer_name in database.\n";
+    }
+    elsif( $num_rows > 1 ){
+        "Cannot retrieve primer from database using primer name, $primer_name!\n";
+    }
+    else{
+        return $primers->[0];
+    }
+}
+
+sub _fetch_primers_by_attributes {
+    my ( $self, $fetch_statement, $attributes ) = @_;
+    my $dbh = $self->connection->dbh();
+    my $primers;
+    my $num_rows = 0;
+    $self->connection->txn(  fixup => sub {
+        my $sth = $dbh->prepare($fetch_statement);
+        $sth->execute( @{$attributes} );
+        
+        while( my @fields = $sth->fetchrow_array ){
+            $num_rows++;
+            my $primer = $self->_make_new_primer_from_db( \@fields );
+            push @{$primers}, $primer;
+        }
+        $sth->finish();
+    } );
+    
+    return ( $primers, $num_rows );
+}
+
+sub _make_new_primer_from_db {
+    my ( $self, $fields, ) = @_;
+    
+    my $primer = Crispr::Primer->new(
+        primer_id => $fields->[0],
+        sequence => $fields->[1],
+        seq_region_name => $fields->[2],
+        seq_region_start => $fields->[3],
+        seq_region_end => $fields->[4],
+        seq_region_strand => $fields->[5],
+        plate_id => $fields->[6],
+        well_id => $fields->[7],
+    );
+    
+    return $primer;
+}
+
+#_build_plate_adaptor
+
+  #Usage       : $crRNAs = $crRNA_adaptor->_build_plate_adaptor();
+  #Purpose     : Internal method to create a new Crispr::DB::PlateAdaptor
+  #Returns     : Crispr::DB::PlatePrepAdaptor
+  #Parameters  : None
+  #Throws      : 
+  #Comments    : 
+
+sub _build_plate_adaptor {
+    my ( $self, ) = @_;
+    return $self->db_connection->get_adaptor( 'plate' );
+}
+
+=method driver
+
+  Usage       : $self->driver();
+  Purpose     : Getter for the db driver.
+  Returns     : Str
+  Parameters  : None
+  Throws      : If driver is not either mysql or sqlite
+  Comments    : 
+
+=cut
+
+=method host
+
+  Usage       : $self->host();
+  Purpose     : Getter for the db host name.
+  Returns     : Str
+  Parameters  : None
+  Throws      : 
+  Comments    : 
+
+=cut
+
+=method port
+
+  Usage       : $self->port();
+  Purpose     : Getter for the db port.
+  Returns     : Str
+  Parameters  : None
+  Throws      : 
+  Comments    : 
+
+=cut
+
+=method dbname
+
+  Usage       : $self->dbname();
+  Purpose     : Getter for the database (schema) name.
+  Returns     : Str
+  Parameters  : None
+  Throws      : 
+  Comments    : 
+
+=cut
+
+=method user
+
+  Usage       : $self->user();
+  Purpose     : Getter for the db user name.
+  Returns     : Str
+  Parameters  : None
+  Throws      : 
+  Comments    : 
+
+=cut
+
+=method pass
+
+  Usage       : $self->pass();
+  Purpose     : Getter for the db password.
+  Returns     : Str
+  Parameters  : None
+  Throws      : 
+  Comments    : 
+
+=cut
+
+=method dbfile
+
+  Usage       : $self->dbfile();
+  Purpose     : Getter for the name of the SQLite database file.
+  Returns     : Str
+  Parameters  : None
+  Throws      : 
+  Comments    : 
+
+=cut
+
+=method connection
+
+  Usage       : $self->connection();
+  Purpose     : Getter for the db Connection object.
+  Returns     : DBIx::Connector
+  Parameters  : None
+  Throws      : 
+  Comments    : 
+
+=cut
+
+=method db_params
+
+  Usage       : $self->db_params();
+  Purpose     : method to return the db parameters as a HashRef.
+                used internally to share the db params around Adaptor objects
+  Returns     : HashRef
+  Parameters  : None
+  Throws      : 
+  Comments    : 
+
+=cut
+
+=method check_entry_exists_in_db
+
+  Usage       : $self->check_entry_exists_in_db( $check_statement, $params );
+  Purpose     : method used to check whether a particular entry exists in the database.
+                Takes a MySQL statement of the form select count(*) from table where condition = ?;'
+                and parameters
+  Returns     : 1 if entry exists, undef if not
+  Parameters  : check statement (Str)
+                statement parameters (ArrayRef[Str])
+  Throws      : 
+  Comments    : 
+
+=cut
+
+=method fetch_rows_expecting_single_row
+
+  Usage       : $self->fetch_rows_expecting_single_row( $sql_statement, $parameters );
+  Purpose     : method to fetch a row from the database where the result should be unique.
+  Returns     : ArrayRef
+  Parameters  : MySQL statement (Str)
+                Parameters (ArrayRef)
+  Throws      : If no rows are returned from the database.
+                If more than one row is returned.
+  Comments    : 
+
+=cut
+
+=method fetch_rows_for_generic_select_statement
+
+  Usage       : $self->fetch_rows_for_generic_select_statement( $sql_statement, $parameters );
+  Purpose     : method to execute a generic select statement and return the rows from the db.
+  Returns     : ArrayRef[Str]
+  Parameters  : MySQL statement (Str)
+                Parameters (ArrayRef)
+  Throws      : If no rows are returned from the database.
+  Comments    : 
+
+=cut
+
+=method _db_error_handling
+
+  Usage       : $self->_db_error_handling( $error_message, $SQL_statement, $parameters );
+  Purpose     : internal method to deal with error messages from the database.
+  Returns     : Throws an exception that depends on the Adaptor type and
+                the error message.
+  Parameters  : Error Message (Str)
+                MySQL statement (Str)
+                Parameters (ArrayRef)
+  Throws      : 
+  Comments    : 
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+1;
+
+__END__
+
+=pod
+ 
+=head1 SYNOPSIS
+ 
+    use Crispr::DB::DBConnection;
+    my $db_connection = Crispr::DB::DBConnection->new(
+        host => 'HOST',
+        port => 'PORT',
+        dbname => 'DATABASE',
+        user => 'USER',
+        pass => 'PASS',
+    );
+  
+    my $primer_adaptor = $db_connection->get_adaptor( 'primer' );
+    
+    # store a primer object in the db
+    $primer_adaptor->store( $primer );
+    
+    # retrieve a primer by id
+    my $primer = $primer_adaptor->fetch_by_id( '214' );
+  
+    # retrieve a primer by combination of type and date
+    my $primer = $primer_adaptor->fetch_by_type_and_date( 'cas9_dnls_native', '2015-04-27' );
+    
+
+=head1 DESCRIPTION
+ 
+ A PrimerAdaptor is an object used for storing and retrieving Primer objects in an SQL database.
+ The recommended way to use this module is through the DBConnection object as shown above.
+ This allows a single open connection to the database which can be shared between multiple database adaptors.
+ 
+=head1 DIAGNOSTICS
+ 
+ 
+=head1 CONFIGURATION AND ENVIRONMENT
+ 
+ 
+=head1 DEPENDENCIES
+ 
+ Moose
+ 
+ Crispr::BaseAdaptor
+ 
+=head1 INCOMPATIBILITIES
+ 
---
+++ t/primer_adaptor.t
@@ -0,0 +1,257 @@
+#!/usr/bin/env perl
+# primer_adaptor.t
+use Test::More;
+use Test::Exception;
+use Test::Warn;
+use Test::DatabaseRow;
+use Test::MockObject;
+use Data::Dumper;
+use autodie qw(:all);
+use Getopt::Long;
+use Readonly;
+use File::Spec;
+
+my $test_data = File::Spec->catfile( 't', 'data', 'test_primer_data.tsv' );
+
+GetOptions(
+    'data=s' => \$test_data,
+);
+
+my $count_output = qx/wc -l $test_data/;
+chomp $count_output;
+$count_output =~ s/\s$test_data//mxs;
+
+Readonly my $TESTS_FOREACH_DBC => 1 + 14 + $count_output * 2 + 2;
+plan tests => 2 * $TESTS_FOREACH_DBC;
+
+use Crispr::DB::DBConnection;
+use Crispr::DB::PrimerAdaptor;
+
+##  database tests  ##
+# Module with a function for creating an empty test database
+# and returning a database connection
+use TestDB;
+# check environment variables have been set
+if( !$ENV{MYSQL_DBNAME} || !$ENV{MYSQL_DBUSER} || !$ENV{MYSQL_DBPASS} ){
+    die "The following environment variables need to be set for connecting to the database!\n",
+        "MYSQL_DBNAME, MYSQL_DBUSER, MYSQL_DBPASS"; 
+}
+
+my %db_connection_params = (
+    mysql => {
+        driver => 'mysql',
+        dbname => $ENV{MYSQL_DBNAME},
+        host => $ENV{MYSQL_DBHOST} || '127.0.0.1',
+        port => $ENV{MYSQL_DBPORT} || 3306,
+        user => $ENV{MYSQL_DBUSER},
+        pass => $ENV{MYSQL_DBPASS},
+    },
+    sqlite => {
+        driver => 'sqlite',
+        dbfile => 'test.db',
+        dbname => 'test',
+    }
+);
+
+# TestDB creates test database, connects to it and gets db handle
+my @db_connections;
+foreach my $driver ( keys %db_connection_params ){
+    push @db_connections, TestDB->new( $db_connection_params{$driver} );
+}
+
+SKIP: {
+    skip 'No database connections available', $TESTS_FOREACH_DBC * 2 if !@db_connections;
+    skip 'Only one database connection available', $TESTS_FOREACH_DBC
+      if @db_connections == 1;
+}
+
+foreach my $db_connection ( @db_connections ){
+    my $driver = $db_connection->driver;
+    my $dbh = $db_connection->connection->dbh;
+
+    # $dbh is a DBI database handle
+    local $Test::DatabaseRow::dbh = $dbh;
+    
+    # make a mock DBConnection object
+    my $mock_db_connection = Test::MockObject->new();
+    $mock_db_connection->set_isa( 'Crispr::DB::DBConnection' );
+    $mock_db_connection->mock( 'dbname', sub { return $db_connection->dbname } );
+    $mock_db_connection->mock( 'connection', sub { return $db_connection->connection } );
+    
+    # get adaptors
+    my $plate_ad = Crispr::DB::PlateAdaptor->new( db_connection => $mock_db_connection, );
+    my $primer_ad = Crispr::DB::PrimerAdaptor->new( db_connection => $mock_db_connection, );
+    # 1 test
+    isa_ok( $primer_ad, 'Crispr::DB::PrimerAdaptor' );
+    
+    # check attributes and methods exist 3 + 11 tests
+    my @attributes = ( qw{ dbname db_connection connection } );
+    
+    my @methods = (
+        qw{ store _split_primer_into_seq_and_tail fetch_by_id fetch_by_name _fetch_primers_by_attributes
+        _make_new_primer_from_db _build_plate_adaptor check_entry_exists_in_db fetch_rows_expecting_single_row fetch_rows_for_generic_select_statement
+            _db_error_handling }
+    );
+
+    foreach my $attribute ( @attributes ) {
+        can_ok( $primer_ad, $attribute );
+    }
+    foreach my $method ( @methods ) {
+        can_ok( $primer_ad, $method );
+    }
+    
+    my $count = 0;
+    # load data into objects
+    open my $fh, '<', $test_data or die "Couldn't open file: $test_data!\n";
+    my ( $l_p_id, $r_p_id, $pair_id ) = ( -1, 0, 0 );
+    while(<$fh>){
+        $count++;
+        chomp;
+        my ( $id, $primer_type, $plate_num, $well_id, $primer_pair_id,
+            $left_primer_name, $left_primer_seq, $right_primer_name, $right_primer_seq, $product_size, ) = split /\s/, $_;
+        
+        # make mock plate object
+        my $plate_suffix = $primer_type eq 'ext'    ?   'd'
+            :               $primer_type eq 'int'   ?   'e'
+            :                                           'ERROR'
+            ;
+        my $mock_plate = Test::MockObject->new();
+        $mock_plate->set_isa('Crispr::Plate');
+        $mock_plate->mock('plate_id', sub { return undef } );
+        $mock_plate->mock('plate_name', sub { return 'CR_00000' . $plate_num . $plate_suffix } );
+        $mock_plate->mock('plate_type', sub { return '96' } );
+        $mock_plate->mock('plate_category', sub { return 'pcr_primers' } );
+        $mock_plate->mock('ordered', sub { return undef } );
+        $mock_plate->mock('received', sub { return undef } );
+        # check whether plate exists in the db, if not add it.
+        my $plate_id;
+        if( $plate_ad->check_entry_exists_in_db( 'select count(*) from plate where plate_name = ?;', [ $mock_plate->plate_name ] ) ){
+            # fetch plate_id
+            $plate_id = $plate_ad->get_plate_id_from_name( $mock_plate->plate_name );
+        }
+        else{
+            $plate_ad->store( $mock_plate );
+            $plate_id = $plate_ad->get_plate_id_from_name( $mock_plate->plate_name );
+        }
+        $mock_plate->mock('plate_id', sub { return $plate_id } );
+        
+        # make mock well object
+        my $mock_well = Test::MockObject->new();
+        $mock_well->set_isa('Labware::Well');
+        $mock_well->mock('plate', sub { return $mock_plate } );
+        $mock_well->mock('position', sub { return $well_id } );
+        
+        # make mock primer and primer pair objects
+        my ( $l_chr, $l_region, $l_strand ) = split /:/, $left_primer_name;
+        my ( $l_start, $l_end, ) = split /-/, $l_region;
+        my $mock_left_primer = Test::MockObject->new();
+        $l_p_id += 2;
+        $mock_left_primer->mock( 'sequence', sub { return $left_primer_seq } );
+        $mock_left_primer->mock( 'seq_region', sub { return $l_chr } );
+        $mock_left_primer->mock( 'seq_region_start', sub { return $l_start } );
+        $mock_left_primer->mock( 'seq_region_end', sub { return $l_end } );
+        $mock_left_primer->mock( 'seq_region_strand', sub { return $l_strand } );
+        $mock_left_primer->set_isa('Crispr::Primer');
+        $mock_left_primer->mock('primer_id', sub { my @args = @_; if( $_[1]){ return $_[1] }else{ return $l_p_id} } );
+        
+        my ( $r_chr, $r_region, $r_strand ) = split /:/, $right_primer_name;
+        my ( $r_start, $r_end, ) = split /-/, $r_region;
+        my $mock_right_primer = Test::MockObject->new();
+        $r_p_id += 2;
+        $mock_right_primer->mock( 'sequence', sub { return $right_primer_seq } );
+        $mock_right_primer->mock( 'seq_region', sub { return $r_chr } );
+        $mock_right_primer->mock( 'seq_region_start', sub { return $r_start } );
+        $mock_right_primer->mock( 'seq_region_end', sub { return $r_end } );
+        $mock_right_primer->mock( 'seq_region_strand', sub { return $r_strand } );
+        $mock_right_primer->set_isa('Crispr::Primer');
+        $mock_right_primer->mock('primer_id', sub { my @args = @_; if( $_[1]){ return $_[1] }else{ return $r_p_id} } );
+        
+        # store left and right primer
+        $mock_well->mock('contents', sub { return $mock_left_primer } );
+        $primer_ad->store( $mock_well );
+        $mock_well->mock('contents', sub { return $mock_right_primer } );
+        $primer_ad->store( $mock_well );
+        
+        # check database rows - 2 tests
+        foreach my $primer ( $mock_left_primer, $mock_right_primer ){
+            row_ok(
+               table => 'primer',
+               where => [ primer_id => $primer->primer_id ],
+               tests => {
+                   'eq' => {
+                        primer_sequence  => $primer->sequence,
+                        primer_tail => undef,
+                        primer_chr => $primer->seq_region,
+                        primer_strand => $primer->seq_region_strand,
+                        well_id => $well_id,
+                   },
+                   '==' => {
+                        primer_start => $primer->seq_region_start,
+                        primer_end => $primer->seq_region_end,
+                        plate_id => $plate_id,
+                   },
+               },
+               label => "$driver: primers stored - $id",
+            );
+        }
+        
+    }
+    
+    my $mock_left_primer = Test::MockObject->new();
+    $l_p_id += 2;
+    $mock_left_primer->mock( 'sequence', sub { return 'ACACTCTTTCCCTACACGACGCTCTTCCGATCTTGGGAGTCCTGCTAATCTCTC' } );
+    $mock_left_primer->mock( 'seq_region', sub { return 4 } );
+    $mock_left_primer->mock( 'seq_region_start', sub { return 60341090 } );
+    $mock_left_primer->mock( 'seq_region_end', sub { return 60341110 } );
+    $mock_left_primer->mock( 'seq_region_strand', sub { return '1' } );
+    $mock_left_primer->set_isa('Crispr::Primer');
+    $mock_left_primer->mock('primer_id', sub { my @args = @_; if( $_[1]){ return $_[1] }else{ return $l_p_id} } );
+    
+    my $mock_right_primer = Test::MockObject->new();
+    $r_p_id += 2;
+    $mock_right_primer->mock( 'sequence', sub { return 'TCGGCATTCCTGCTGAACCGCTCTTCCGATCTCACAGCACTGTATATAAACAGTG' } );
+    $mock_right_primer->mock( 'seq_region', sub { return 4 } );
+    $mock_right_primer->mock( 'seq_region_start', sub { return 60341311 } );
+    $mock_right_primer->mock( 'seq_region_end', sub { return 60341333 } );
+    $mock_right_primer->mock( 'seq_region_strand', sub { return '-1' } );
+    $mock_right_primer->set_isa('Crispr::Primer');
+    $mock_right_primer->mock('primer_id', sub { my @args = @_; if( $_[1]){ return $_[1] }else{ return $r_p_id} } );
+    
+    # store primers
+    $primer_ad->store( $mock_left_primer );
+    $primer_ad->store( $mock_right_primer );
+    
+    # check database rows - 2 tests
+    foreach my $primer ( $mock_left_primer, $mock_right_primer ){
+        my $primer_seq = $primer->sequence;
+        my $primer_tail;
+        if( $primer_seq =~ m/\A ACACTCTTTCCCTACACGACGCTCTTCCGATCT/xms ){
+            $primer_seq =~ s/\A ACACTCTTTCCCTACACGACGCTCTTCCGATCT//xms;
+            $primer_tail = 'ACACTCTTTCCCTACACGACGCTCTTCCGATCT';
+        }
+        elsif( $primer_seq =~ m/\A TCGGCATTCCTGCTGAACCGCTCTTCCGATCT/xms){
+            $primer_seq =~ s/\A TCGGCATTCCTGCTGAACCGCTCTTCCGATCT//xms;
+            $primer_tail = 'TCGGCATTCCTGCTGAACCGCTCTTCCGATCT';
+        }
+        row_ok(
+           table => 'primer',
+           where => [ primer_id => $primer->primer_id ],
+           tests => {
+               'eq' => {
+                    primer_sequence  => $primer_seq,
+                    primer_tail => $primer_tail,
+                    primer_chr => $primer->seq_region,
+                    primer_strand => $primer->seq_region_strand,
+                    well_id => undef,
+               },
+               '==' => {
+                    primer_start => $primer->seq_region_start,
+                    primer_end => $primer->seq_region_end,
+                    plate_id => undef,
+               },
+           },
+           label => "$driver: primers stored - with tail",
+        );
+    }
+    $db_connection->destroy();
+}
