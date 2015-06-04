0e66cd2626eba301020743c2c9ab12d76413ca67
Richard White <richard.white@sanger.ac.uk>
2015-06-04 16:15:59 +0100

    New File: crispr_results_tile_plots.R script to display the results of an analysis graphically. Diplays total percentage of indels in each sample in a 96/384 well plate format.
    New File: plate_well_functions.R
    Accessory file with plate-based functions

---
+++ scripts/crispr_results_tile_plots.R
@@ -0,0 +1,185 @@
+#!/usr/bin/env Rscript
+
+# load packages
+library(ggplot2)
+library(plyr)
+library(scales)
+library(optparse)
+
+option_list <- list(
+  make_option(c("-d", "--directory"), type="character", default='cwd',
+              help="Working directory [default %default]" ),
+  make_option("--figure_type", type="character", default='pdf',
+              help = "Type of figures to generate - pdf | png [default %default]"),
+  make_option("--scripts_directory", type="character",  default='/nfs/users/nfs_r/rw4/checkouts/Crispr/scripts',
+              help="directory where plate_well_functions is located [default %default]"),
+  make_option("--basename", type="character", default='mpx',
+              help="A base name for all output files [default %default]"),
+  make_option("--plate_type", type="character", default='96',
+              help="Type of plate to plot - 96 | 384 [default %default]"),
+  make_option(c("-v", "--verbose"), action="store_true", default=TRUE,
+              help="Print extra output [default]"),
+  make_option(c("-q", "--quietly"), action="store_false",
+              dest="verbose", help="Print little output")
+)
+
+cmd_line_args <- parse_args(OptionParser(option_list=option_list), positional_arguments = 1 )
+
+if( cmd_line_args$options[['directory']] == 'cwd' ){
+	cmd_line_args$options[['directory']] <- getwd()
+}
+if( cmd_line_args$options[['verbose']] ){
+	cat( "Working directory:", cmd_line_args$options[['directory']], "\n", sep=" " )
+	cat( "Figure Type:", cmd_line_args$options[['figure_type']], "\n", sep=" " )
+	cat( "Scripts directory:", cmd_line_args$options[['scripts_directory']], "\n", sep=" " )
+	cat( "Output files basename:", cmd_line_args$options[['basename']], "\n", sep=" " )
+	cat( "Plate Type:", cmd_line_args$options[['plate_type']], "\n", sep=" " )
+}
+
+plate_well_file <- file.path( cmd_line_args$options[['scripts_directory']], 'plate_well_functions.R' )
+if( !file.exists(plate_well_file) ){
+	# try current working directory
+	plate_well_file <- file.path( getwd(), 'plate_well_functions.R' )
+	cat(plate_well_file, "\n")
+	if( !file.exists(plate_well_file) ){
+		stop("Couldn't find plate well functions file\n", plate_well_file)
+	}
+}
+
+# source plate well functions
+source(plate_well_file)
+
+# set working directory
+setwd( cmd_line_args$options[['directory']] )
+
+# open data file
+input_file <- cmd_line_args$args[1]
+
+col_names <- c("plex", "plate", "sub_plex", "well", "sample_name", "gene", "group_name", "region", "caller", "indel_position",
+"crispr_name", "chr", "pos", "ref", "alt", "reads", "total_on_target_reads", "pc_total_reads",
+"consensus_start", "ref_seq", "alt_consensus" )
+data_types <- c("factor","factor","factor","factor","factor","factor","factor","factor","factor","factor",
+"factor", "character","integer","character","character","integer","integer","numeric",
+"numeric", "character", "character" )
+
+all_indels <- read.table(file=input_file, sep="\t", comment.char="%", col.names=col_names, colClasses=data_types )
+all_indels$group_name <- factor( all_indels$group_name, levels= order( levels( all_indels$group_name ) ) )
+
+# remove non-overlapping
+indels <- subset( all_indels, ( is.na( all_indels$indel_position ) | all_indels$indel_position == 'crispr' | all_indels$indel_position == 'crispr_pair' ) )
+# remove indels below 0.1%
+# indels <- subset( indels, indels$pc_total_reads >= 0.001 )
+
+# add well info
+indels$row <- well_to_row( indels$well )
+indels$column <- well_to_column( indels$well )
+indels <- add_row_indices_for_tile_plot( indels )
+
+# summaries
+results_by_sample_by_crispr <- ddply(indels, .(plex,plate,sub_plex,sample_name,crispr_name), summarise,
+	region = region[1],
+	gene = gene[1],
+	group_name = group_name[1],
+	total_indels=length(reads), 
+	Deletions=sum(nchar(ref) > nchar(alt)),
+	Insertions=sum(nchar(ref) < nchar(alt)),
+	pc_reads=round( sum(pc_total_reads, na.rm=TRUE) * 100, 1 ),
+	pc_reads_per_indel=round( sum(pc_total_reads, na.rm=TRUE)/( length(reads) - 1 ) * 100, 1 ),
+	pc_major_variant=round( max(pc_total_reads, na.rm=TRUE) * 100, 1 ), 
+	total_on_target_reads = total_on_target_reads[1],
+	row=row[1], col=column[1], row_i = row_indices[1] )
+
+results_by_sample_by_crispr$total_indels[ results_by_sample_by_crispr$pc_reads == 0 ] <- 0
+
+results_by_gene <- ddply(results_by_sample_by_crispr, .(plex,plate,sub_plex,crispr_name,gene), summarise,
+	region = region[1],
+	gene = gene[1],
+	group_name = group_name[1],
+	min_pc = round( min( pc_reads ), digits=1 ),
+	max_pc = round( max( pc_reads ), digits=1 ),
+	mean_pc = round( mean( pc_reads ), digits=1 ),
+	sd_pc = round( sd( pc_reads ), digits=1 ),
+	num_founders_over_5pc = sum( pc_reads > 5 ),
+	max_pc_major_variant = round( max( pc_major_variant ), digits=1 )
+)
+
+# sort and output table
+results_sorted <- results_by_sample_by_crispr[ order( results_by_sample_by_crispr$plate, -results_by_sample_by_crispr$row_i, results_by_sample_by_crispr$col ), ]
+results_sorted_subset <- subset( results_sorted, select = c("plex", "plate", "sub_plex", "sample_name", "region", "gene", "crispr_name", "total_indels", "pc_reads", "pc_major_variant", "total_on_target_reads"))
+table_filename <- paste(cmd_line_args$options[['basename']], "results.txt", sep=".")
+write.table(results_sorted_subset, file=table_filename, col.names = TRUE, row.names = FALSE, quote = FALSE, sep="\t" )
+
+# output summary table
+summary_table <- subset(results_by_gene, select = c("sub_plex", "gene", "region", "crispr_name", "min_pc", "max_pc", "mean_pc", "sd_pc", "num_founders_over_5pc", "max_pc_major_variant" ) )
+summary_filename <- paste(cmd_line_args$options[['basename']], "summary.txt", sep=".")
+write.table(summary_table, file=summary_filename, col.names = TRUE, row.names = FALSE, quote = FALSE, sep="\t" )
+
+# coverage
+log_labels <- c("10", "100", "1000", "10000", "100000", "1000000")
+coverage_plot <- ggplot(data=results_by_sample_by_crispr) + 
+	geom_boxplot( aes(y = total_on_target_reads, x = sub_plex ) ) +
+	scale_y_log10( limits = c(10,1000000), breaks = as.numeric(log_labels), labels = log_labels ) +
+	labs(x = "Target", y = "Coverage" )
+
+# on target stats
+# check if file exists
+total_reads_file <- paste(cmd_line_args$options[['basename']], "total_reads.txt", sep=".")
+plot_on_target_plot <- FALSE
+if( file.exists(total_reads_file) ){
+	total_reads <- read.table(file=total_reads_file, sep="\t", col.names = c("plex","sample_name","total_reads"))
+	
+	# merge data frames
+	results_by_sample_by_gene <- merge(results_by_sample_by_gene, total_reads)
+	
+	# create new ON target % column
+	results_by_sample_by_gene$pc_on_target <- results_by_sample_by_gene$total_on_target_reads / 	results_by_sample_by_gene$total_reads
+	
+	# make boxplot of ON target %
+	ON_target_boxplot <- ggplot( data=results_by_sample_by_gene ) + 
+		geom_boxplot( aes( x = plex, y = pc_on_target ) ) + 
+		scale_y_continuous( limits = c(0,1), labels = percent ) + 
+		labs(x = "Target", y = "Percentage ON Target")
+	
+	plot_on_target_plot <- TRUE
+}
+
+# plot tile plots
+plot_list <- by(results_by_sample_by_crispr, results_by_sample_by_crispr$group_name,
+		plate_tile_plot, summary_results, plate_type=cmd_line_args$options[['plate_type']], simplify=FALSE)
+
+# print to files
+if( cmd_line_args$options[['figure_type']] == 'pdf' ){
+	file_name <- paste(cmd_line_args$options[['basename']], '.pdf', sep='')
+	pdf(file=file_name,onefile=TRUE,width=9,height=6,paper="special") 
+	
+	print(coverage_plot)
+	
+	if( plot_on_target_plot ){
+		print(ON_target_boxplot)
+	}
+	
+	for( plate in 1:length(plot_list) ){
+		print(plot_list[[plate]])
+	}
+	
+	dev.off()
+}else if( cmd_line_args$options[['figure_type']] == 'png' ){
+	file_name <- paste( cmd_line_args$options[['basename']], 'coverage', 'png', sep='.' )
+	png(filename=file_name, width=720, height=480 )
+	print(coverage_plot)
+	dev.off()
+	
+	if( plot_on_target_plot ){
+		file_name <- paste( cmd_line_args$options[['basename']], 'on_target', 'png', sep='.' )
+		png(filename=file_name, width=720, height=480 )
+		print(ON_target_boxplot)
+		dev.off()
+	}
+	
+	for( group in 1:length(plot_list) ){
+		file_name <- paste( cmd_line_args$options[['basename']], paste('group', group, sep='_' ), 'png', sep='.' )
+		png(filename=file_name, width=720, height=480 )
+		print(plot_list[[group]])
+		dev.off()
+	}
+}
---
+++ scripts/plate_well_functions.R
@@ -0,0 +1,138 @@
+well_to_row <- function( well_vector ){
+	return( as.factor(substr(well_vector, 1, 1)) )
+}
+
+well_to_column <- function( well_vector ){
+	columns <- substr(well_vector, 2, 3)
+	columns <- as.integer( sub("^0", "", columns, perl=TRUE ) )
+	return( columns )
+}
+
+add_row_indices_for_tile_plot <- function(data_frame) {
+	# check object is a data.frame and check that it has a row column
+	if( class(data_frame) != "data.frame" ){
+		stop("Supplied object is not a data frame")
+	}
+	if( sum( grepl("row", colnames(data_frame) ) ) == 0 ){
+		stop("There is no column labelled 'row'.")
+	}
+	
+	row_names <- LETTERS[1:16]
+	row_i <- c( seq(-1,-16) )
+	row_indices <- vector(length=nrow(data_frame))
+	for( letter in row_names ){
+		row_indices[ data_frame$row == letter ] <- row_i[row_names == letter]
+	}
+	data_frame$row_indices <- as.integer(row_indices)
+	return( data_frame )
+}
+
+
+plate_tile_plot <- function( data_frame, summary_results, palette = "Reds", plate_type = '96' ){
+	# check data_frame is a data.frame and check that it has the right columns
+	if( class(data_frame) != "data.frame" ){
+		stop("Supplied object is not a data frame")
+	}
+	for( column_name in c("plex", "plate", "group_name", "row_i", "col", "pc_reads", "total_on_target_reads") ){
+		if( sum( grepl(column_name, colnames(data_frame) ) ) == 0 ){
+			stop( paste("There is no column labelled '", column_name, "'.", sep="") )
+		}
+	}
+	
+	# change settings based on plate_type
+	if( plate_type == '96' ){
+		last_col <- 12
+		last_row_i <- 8
+		pcs_text_size <- 4
+		reads_text_size <- 3
+	}else{
+		last_col <- 24
+		last_row_i <- 16
+		pcs_text_size <- 2.5
+		reads_text_size <- 2
+	}
+	
+	# set scale_limits
+	scale_limits <- c( 0, 100 )
+	
+	data_frame$row_i <- as.integer(data_frame$row_i)
+	data_frame$col <- as.integer(data_frame$col)
+	
+	# prepare text for plotting. 
+	pc_reads_text <- subset(data_frame, select = c(col, row_i, pc_reads ))
+	# Needs to be offset from center of square to fit both in
+	pc_reads_text$row_i <- pc_reads_text$row_i + 0.25 
+	pc_reads_text$pc_reads <- as.character(pc_reads_text$pc_reads)
+
+	# same for total reads text
+	total_reads_text <- subset(data_frame, select = c(col, row_i, total_on_target_reads))
+	total_reads_text$total_on_target_reads <- as.character(total_reads_text$total_on_target_reads)
+	# add brackets
+#	total_reads_text$total_on_target_reads <- sub("^", "(", total_reads_text$total_on_target_reads, perl=TRUE)
+#	total_reads_text$total_on_target_reads <- sub("$", ")", total_reads_text$total_on_target_reads, perl=TRUE)
+	# shift text down to fit into box
+	total_reads_text$row_i <- total_reads_text$row_i - 0.25 
+	
+	# go through wells and add line if it doesn't exist
+	for( column in 1:last_col ){
+		for( row_num in -1:-(last_row_i) ){
+			if( nrow( data_frame[ data_frame$col == column & data_frame$row_i == row_num, ] ) == 0 ){
+				new_row <- data.frame( plex=data_frame$plex[1],
+										plate=data_frame$plate[1],
+										sub_plex=NA,
+										sample_name=NA,
+										crispr_name=NA,
+										region=NA,
+										gene=NA,
+										group_name=data_frame$group_name[1],
+										total_indels=0,
+										Deletions=0,
+										Insertions=0,
+										pc_reads=NA,
+										pc_reads_per_indel=NA,
+										pc_major_variant=NA,
+										total_on_target_reads=NA,
+										row=NA,
+										col=column,
+										row_i=row_num )
+				data_frame <- rbind(data_frame, new_row)
+			}
+		}
+	}
+
+	# create title text
+	title_text <- paste("Number of indels per embryo (", data_frame$plex[1], " - plate ", data_frame$plate[1],
+						", group ", data_frame$group_name[1], ")", sep="" )
+	
+	# sort out colours
+	plot_colours <- character()
+	if( missing(palette) ){
+		plot_colours <- c( "white", "#cb181d" )
+	}else if( palette == "Blues"){
+		plot_colours <- c( "white", "#2171b5" )
+	}else if( palette == "Reds"){
+		plot_colours <- c( "white", "#cb181d" )
+	}else{
+		plot_colours <- c( "white", "#cb181d" )
+	}
+	
+#	# make legend
+#	legend_table <- subset( summary_results, summary_results$group == data_frame$group_name[1], select=c(group, sub_plex, gene_name ) )
+	
+	row_labels <- LETTERS[1:last_row_i]
+	row_breaks <- c(seq(-1, -last_row_i))
+	col_breaks <- c(seq(1, last_col))
+	# make plot
+	plate_plot <- ggplot(data_frame, aes(x=col,y=row_i)) + geom_tile(aes(fill=pc_reads)) + 
+			scale_fill_continuous(low = plot_colours[1], high = plot_colours[2], space = "Lab", 
+				na.value = "grey90", guide = "colourbar", limits = scale_limits) + 
+			geom_text(data=pc_reads_text, aes(label=pc_reads), size = pcs_text_size ) + 
+			geom_text(data=total_reads_text, aes(label=total_on_target_reads), size = reads_text_size ) + 
+#			annotation_custom(tableGrob(legend_table), xmin=13, xmax=15, ymin=-6, ymax=-8) +
+			scale_y_continuous(breaks=row_breaks, labels=row_labels ) + 
+			scale_x_continuous(breaks=col_breaks) + 
+			labs(y="Rows", x="Columns", title=title_text)
+	
+	return( plate_plot )
+}
+
