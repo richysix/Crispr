33a1b6f6d059bb1c10605e7dff31ac3a54384089
Richard White <richard.white@sanger.ac.uk>
2016-01-29 09:26:24 +0000

    t/lib/TestMethods.pm Added code to force mock genome to be indexed

--- t/lib/TestMethods.pm
+++ t/lib/TestMethods.pm
@@ -79,6 +79,8 @@ sub check_for_test_genome {
                                   -file => ">$genome_file_path", );
         $out->write_seq($chr3);
     }
+    # force mock genome to get reindexed
+    my $db = Bio::DB::Fasta->new( $genome_file_path, -reindex );
     
     # check whether bwa index exists
     my $bwa_index_file = File::Spec->catfile( 't/data', $genome_file . '.bwt' );
